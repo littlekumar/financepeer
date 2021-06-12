74dfe18fdd7fad1948184412940c534628381112
littlekumar <57680556+littlekumar@users.noreply.github.com>
2021-06-12 10:12:30 +0530

    first commit

--- load_json_files/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!H:\intr\interview_tasks\financepeer\load_json_files\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- load_json_files/load_json_files/settings.py
+++ load_json_files/load_json_files/settings.py
@@ -111,7 +111,7 @@ WSGI_APPLICATION = 'load_json_files.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db3.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
